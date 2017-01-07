79b8a15e572a9cc0dad9e3e253741304a2557548
Vittorio <vittorio.vaselli@outlook.it>
2017-01-07 19:10:34 +0100

    Use local and roaming settings

--- Utilities/Utils/SettingsManager.cs
+++ Utilities/Utils/SettingsManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Foundation.Collections;
 using Windows.Storage;
 
 namespace Utilities
@@ -10,21 +11,45 @@ namespace Utilities
     public class SettingsManager
     {
         
-        public static void Save<T>(string key, T value)
+
+        public static void SaveLocal<T>(string key, T value)
+        {
+            var values = ApplicationData.Current.LocalSettings.Values;
+            Save<T>(key, value, values);
+        }
+
+        public static void SaveInRoaming<T>(string key, T value)
         {
             var values = ApplicationData.Current.RoamingSettings.Values;
+            Save<T>(key, value, values);
+        }
+
+        public static void Save<T>(string key, T value, IPropertySet values)
+        {
             if (values.ContainsKey(key))
                 values.Remove(key);
             values.Add(key, value);
         }
 
-        public static T Get<T>(string key)
+        public static T GetLocal<T>(string key)
         {
-            var values = ApplicationData.Current.RoamingSettings.Values;
+            var values = ApplicationData.Current.LocalSettings.Values;
             return (T)values[key];
         }
 
-        public static bool ContainsKey(string key)
+        public static T GetInRoaming<T>(string key)
+        {
+            var values = ApplicationData.Current.LocalSettings.Values;
+            return (T)values[key];
+        }
+
+        public static bool ContainsKeyLocal(string key)
+        {
+            var values = ApplicationData.Current.LocalSettings.Values;
+            return values.ContainsKey(key);
+        }
+
+        public static bool ContainsKeyInRoaming(string key)
         {
             var values = ApplicationData.Current.RoamingSettings.Values;
             return values.ContainsKey(key);
@@ -36,7 +61,7 @@ namespace Utilities
             {
                 return false;
             }
-            Save<bool>(SettingsKeys.FIRSTSTART, true);
+            SaveLocal<bool>(SettingsKeys.FIRSTSTART, true);
             return true;
         }
 
