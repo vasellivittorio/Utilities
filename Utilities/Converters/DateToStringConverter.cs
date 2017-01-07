27ac83a0ae31c2835f23a4e1ca31a90b5bff36f3
Vittorio <vittorio.vaselli@outlook.it>
2017-01-07 22:32:51 +0100

    Add DateToStringConverter

---
+++ Utilities/Converters/DateToStringConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace Utilities.Converters
+{
+    public class DateToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            //ConverterParameter=\{0:dd MMMM yyyy\}
+            string formatString = parameter as string;
+            if (!string.IsNullOrEmpty(formatString))
+            {
+                return string.Format(formatString, value);
+            }
+
+            return value.ToString();
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
